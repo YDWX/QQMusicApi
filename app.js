308d2393132b8eda4e3d3a38b3afc0fa7a450b4f
nonym <nonym@qq.com>
2019-08-31 07:13:54 +0800

    enclosure request-p and add cache, cors to app.js

--- app.js
+++ app.js
@@ -4,10 +4,25 @@ var path = require('path')
 var cookieParser = require('cookie-parser')
 var lessMiddleware = require('less-middleware')
 var logger = require('morgan')
+const cache = require('apicache').middleware
 
 const initRoutes = require('./initRoutes')
 
 var app = express()
+
+// CORS
+app.use((req, res, next) => {
+  if(req.path !== '/' && !req.path.includes('.')){
+    res.header({
+      'Access-Control-Allow-Credentials': true,
+      'Access-Control-Allow-Origin': req.headers.origin || '*',
+      'Access-Control-Allow-Headers': 'X-Requested-With',
+      'Access-Control-Allow-Methods': 'PUT,POST,GET,DELETE,OPTIONS',
+      'Content-Type': 'application/json; charset=utf-8'
+    })
+  }
+  next()
+})
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'))
@@ -20,11 +35,20 @@ app.use(cookieParser())
 app.use(lessMiddleware(path.join(__dirname, 'public')))
 app.use(express.static(path.join(__dirname, 'public')))
 
+// cache
+app.use(cache('2 minutes', ((req, res) => res.statusCode === 200)))
+
+// cookie处理
+// app.use((req, res, next) => {
+//   res.cookie('test','qwer')
+//   next()
+// })
+
 // 初始化路由，自动读取routes目录下的所有路由
 initRoutes(app)
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use((req, res, next) => {
   next(createError(404))
 })
 
---
+++ util/request.js
@@ -0,0 +1,92 @@
+/* eslint-disable no-prototype-builtins */
+/* 这是对request-promise进行封装
+ * 该系统自定义接口参数与QQ音乐API文档参数做转换
+ * 默认参数不需要用户设定，自动带上
+ * 默认参数有：loginUin=0 hostUin=0 inCharset=utf8 
+ * outCharset=utf-8 platform=yqq needNewCode=0
+*/
+const rp = require('request-promise')
+// 预设参数(为GET请求设置)
+let preParams = {
+  loginUin: 0,
+  hostUin: 0,
+  inCharset: 'utf8',
+  outCharset: 'utf-8',
+  platform: 'yqq',
+  needNewCode: 0,
+  format: 'json'
+}
+
+const UAGenerator = () => {
+  const userAgentList = [
+    'Mozilla/5.0 (iPhone; CPU iPhone OS 9_1 like Mac OS X) AppleWebKit/601.1.46 (KHTML, like Gecko) Version/9.0 Mobile/13B143 Safari/601.1',
+    'Mozilla/5.0 (iPhone; CPU iPhone OS 9_1 like Mac OS X) AppleWebKit/601.1.46 (KHTML, like Gecko) Version/9.0 Mobile/13B143 Safari/601.1',
+    'Mozilla/5.0 (Linux; Android 5.0; SM-G900P Build/LRX21T) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/59.0.3071.115 Mobile Safari/537.36',
+    'Mozilla/5.0 (Linux; Android 6.0; Nexus 5 Build/MRA58N) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/59.0.3071.115 Mobile Safari/537.36',
+    'Mozilla/5.0 (Linux; Android 5.1.1; Nexus 6 Build/LYZ28E) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/59.0.3071.115 Mobile Safari/537.36',
+    'Mozilla/5.0 (iPhone; CPU iPhone OS 10_3_2 like Mac OS X) AppleWebKit/603.2.4 (KHTML, like Gecko) Mobile/14F89;GameHelper',
+    'Mozilla/5.0 (iPhone; CPU iPhone OS 10_0 like Mac OS X) AppleWebKit/602.1.38 (KHTML, like Gecko) Version/10.0 Mobile/14A300 Safari/602.1',
+    'Mozilla/5.0 (iPad; CPU OS 10_0 like Mac OS X) AppleWebKit/602.1.38 (KHTML, like Gecko) Version/10.0 Mobile/14A300 Safari/602.1',
+    'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.12; rv:46.0) Gecko/20100101 Firefox/46.0',
+    'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/59.0.3071.115 Safari/537.36',
+    'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_5) AppleWebKit/603.2.4 (KHTML, like Gecko) Version/10.1.1 Safari/603.2.4',
+    'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:46.0) Gecko/20100101 Firefox/46.0',
+    'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/51.0.2704.103 Safari/537.36',
+    'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/42.0.2311.135 Safari/537.36 Edge/13.10586'
+  ]
+  return userAgentList[Math.round(Math.random()*13)]
+}
+const methods = (()=>{
+  const arr = ['GET', 'POST', 'HEAD', 'PUT', 'OPTIONS', 'DELETE', 'CONNECT', 'TRACE', 'PATCH']
+  const res = {}
+  arr.forEach((method)=>{
+    res[method] = 1
+  })
+  return res
+})()
+
+/**
+ *
+ *
+ * @param {*} method 方法
+ * @param {*} url 请求路径
+ * @param {*} headers 包含header信息
+ * @param {*} data 请求参数数据
+ */
+const createRequest = (method, uri, headers, data) => {
+  method = method.toUpperCase()
+  method = methods.hasOwnProperty(method)? method:'GET'
+  // headers 中包含较重要的头信息例如User-Agent、Referer、Cookie
+  if (!headers['User-Agent']){
+    headers['User-Agent'] = UAGenerator()
+  }
+  headers['Referer'] = 'https://c.y.qq.com/'
+
+
+  const options = {
+    method,
+    uri,
+    headers,
+  }
+  Object.assign(data, preParams)
+  if (method==='GET') {
+    options['qs'] = data
+  }else {
+    options['body'] = data
+  }
+  const resp = {
+    status: 200,
+    body: {},
+  }
+  return new Promise((resolve, reject) => {
+    rp(options).then((res)=>{
+      resp.body = {code: 200, data: res}
+      resolve(resp)
+    }).catch((err)=>{
+      resp.status = 500
+      resp.body = {code: 500, msg: err, data: null}
+      reject(resp)
+    })
+  })
+}
+module.exports = createRequest
