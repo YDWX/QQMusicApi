84e44c60d2941f2642f6d63d5c304566a7c4a210
nonym <nonym@qq.com>
2019-09-02 20:41:40 +0800

    热搜接口

---
+++ routes/hot_search.js
@@ -0,0 +1,17 @@
+const consRouter = (router, request) => {
+  const uri = 'https://c.y.qq.com/splcloud/fcgi-bin/gethotkey.fcg'
+  const data = {
+    g_tk: 5381,
+    _: (new Date()).getTime()
+  }
+  router.get('/', (req, res, next) => {
+    request('GET', uri, {}, data).then((resp)=>{
+      res.status(200).json(resp.body)
+    }).catch((err)=>{
+      console.log(err)
+    })
+  })
+  return router
+}
+
+module.exports = consRouter
