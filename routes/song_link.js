5b85914d654e1b0fc570b8465e9e74002f152468
nonym <nonym@qq.com>
2019-12-23 18:54:10 +0800

    修复 songlink的代码错误

--- routes/song_link.js
+++ routes/song_link.js
@@ -1,20 +1,13 @@
 // 获取歌曲文件地址
 const vkey_service = require('../service/vkey')
 const consRouter = (router, request) => {
-  const uri = 'https://c.y.qq.com/base/fcgi-bin/fcg_music_express_mobile3.fcg'
-  const data = {
-    cid: 205361747,
-    songmid: '',
-    filename: '',
-    guid: 3655047200
-  }
+
   router.get('/', (req, res, next) => {
     let {songmid} = req.query
     if (!songmid) {
       res.status(400).json({code: 400, data: null, msg: 'no keyword albummid'})
     }
     const filename = `C400${songmid}.m4a`
-    Object.assign(data, {songmid, filename})
     vkey_service(songmid).then((data)=>{
       let file_resp = null
       if (!data.body.data.items[0].vkey){
@@ -29,7 +22,7 @@ const consRouter = (router, request) => {
         file_resp = {
           code: 200,
           data: {
-            url: `http://dl.stream.qqmusic.qq.com/${filename}?vkey=${resp.body.data.items[0].vkey}&guid=${data.guid}&fromtag=66`
+            url: `http://dl.stream.qqmusic.qq.com/${filename}?vkey=${data.body.data.items[0].vkey}&guid=${data.guid}&fromtag=66`
           },
           msg: 'get success'
         }
